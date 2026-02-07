04ebe12f4f321ce8df771c2f1e766847bcac5131
Ulibaykin <nebaykin.sergey@gmail.com>
2026-02-07 23:43:22 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,16 @@
 export const metadata = {
-  title: 'Goals 2026',
-  description: 'Личный трекер целей на 2026 год',
-}
+  title: "Goals 2026",
+  description: "Личный трекер целей на 2026 год",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="ru">
       <body>{children}</body>
     </html>
-  )
+  );
 }
